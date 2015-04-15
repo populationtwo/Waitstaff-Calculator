3598d084fd09e2438103f54843e1766b8e44d51e
Edwin Lunandy <populationtwo@gmail.com>
2015-04-15 15:46:00 -0400

    Add calculation logic.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,27 @@
-angular.module('myApp',[])
-	.controller('calcController', function($scope){
-		$scope.mealData = {}
-
-
+angular.module( 'myApp', [] )
+	.controller( 'calcController', function ($scope) {
+		$scope.mealData = {};
+		$scope.chargeData = {};
+		$scope.earningData = {};
 
 		// validate data
-		$scope.mealDetailSubmit = function() {
-			if($scope.waitStaffForm.$valid){
+		$scope.mealDetailSubmit = function () {
+			if ($scope.waitStaffForm.$valid) {
 				// do calculation
 				calculate();
 			}
 		}
 
 		// calculate
-		var calculate = function(){
-
-			// chargedata
-			//subtotal = base meal price + basemeal price x tax
-			//tip = subtotal * tip rate
-			//
-			//total = subtotal + tip
-
-
+		var calculate = function () {
+			$scope.chargeData.subTotal = $scope.mealData.mealPrice + ($scope.mealData.mealPrice * $scope.mealData.taxRate / 100 );
+			$scope.chargeData.tip = $scope.chargeData.subTotal * $scope.mealData.tipPercentage / 100;
+			$scope.chargeData.total = $scope.chargeData.tip + $scope.chargeData.subTotal;
 
 
 
 			//earnings info
-			//
+			$scope.earningData.tipTotal += $scope.chargeData.tip;
 			//tip total = tip
 			//meal count = number of meal
 			//average tip = tip total / meal count
@@ -34,4 +29,4 @@ angular.module('myApp',[])
 		}
 
 
-	})
+	} )
