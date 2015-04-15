b2071c84308ca13370ccac20e39d33e8f88be695
Edwin Lunandy <populationtwo@gmail.com>
2015-04-15 15:16:51 -0400

    Add controller.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
+angular.module('myApp',[])
+	.controller('calcController', function($scope){
+		$scope.mealData = {}
 
+
+
+		// validate data
+		$scope.mealDetailSubmit = function() {
+			if($scope.waitStaffForm.$valid){
+				// do calculation
+				calculate();
+			}
+		}
+
+		// calculate
+		var calculate = function(){
+
+			// chargedata
+			//subtotal = base meal price + basemeal price x tax
+			//tip = subtotal * tip rate
+			//
+			//total = subtotal + tip
+
+
+
+
+
+			//earnings info
+			//
+			//tip total = tip
+			//meal count = number of meal
+			//average tip = tip total / meal count
+
+		}
+
+
+	})
