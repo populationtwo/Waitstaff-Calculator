7273d7413dd28a079edb498924455fd121568bc6
Edwin Lunandy <populationtwo@gmail.com>
2015-04-16 14:10:07 -0400

    Refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 angular.module( 'myApp', [] )
 	.controller( 'calcController', function ($scope) {
-		$scope.mealData = {};
-		$scope.chargeData = {};
-		$scope.earningData = {};
+
+		// set initial value
+		var init = function () {
+
+			$scope.mealData = {};
+			$scope.chargeData = {};
+			$scope.earningData = {};
+
+			$scope.earningData.tipTotal = 0;
+			$scope.earningData.mealCount = 0;
+			$scope.earningData.tipMean = 0;
+
+			$scope.chargeData.subTotal = 0;
+			$scope.chargeData.tip = 0;
+			$scope.chargeData.total = 0;
+
+			$scope.formSubmitted = false;
+
+		}
+		init();
 
 		// validate data
 		$scope.mealDetailSubmit = function () {
@@ -14,19 +31,29 @@ angular.module( 'myApp', [] )
 
 		// calculate
 		var calculate = function () {
+			//Customer charges info
 			$scope.chargeData.subTotal = $scope.mealData.mealPrice + ($scope.mealData.mealPrice * $scope.mealData.taxRate / 100 );
 			$scope.chargeData.tip = $scope.chargeData.subTotal * $scope.mealData.tipPercentage / 100;
 			$scope.chargeData.total = $scope.chargeData.tip + $scope.chargeData.subTotal;
 
-
-
-			//earnings info
+			//My Earnings info
 			$scope.earningData.tipTotal += $scope.chargeData.tip;
-			//tip total = tip
-			//meal count = number of meal
-			//average tip = tip total / meal count
+			$scope.earningData.mealCount++
+			$scope.earningData.tipMean = $scope.earningData.tipTotal / $scope.earningData.mealCount;
+		}
+
+		// reset all values
+		$scope.resetForm = function () {
+			init();
 
 		}
 
-
+		// reset meal deatils and customer charges
+		$scope.cancelForm = function () {
+			$scope.chargeData.subTotal = 0;
+			$scope.chargeData.tip = 0;
+			$scope.chargeData.total = 0;
+			$scope.mealData = {};
+			$scope.formSubmitted = false;
+		}
 	} )
