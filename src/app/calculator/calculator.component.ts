e8bcfff6c0d458624b070ff80971107d28774f1f
populationtwo <populationtwo@gmail.com>
2019-06-09 20:19:09 -0400

    Create earnings component.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,6 +10,9 @@ export class CalculatorComponent implements OnInit {
     subtotal: number = 0;
     tip: number = 0;
     total: number = 0;
+    earningsTotal: number = 0;
+    mealCount: number = 0;
+    averageTip: number = 0;
 
 
     constructor() {
@@ -26,6 +29,15 @@ export class CalculatorComponent implements OnInit {
         this.subtotal = formValue.price * ((formValue.taxRate + 100) / 100);
         this.tip = this.subtotal * formValue.tipRate / 100;
         this.total = this.subtotal + this.tip;
+
+        this.earningsTotal = this.earningsTotal + this.tip;
+        this.mealCount = this.mealCount + 1;
+        this.averageTip = this.earningsTotal / this.mealCount;
     }
 
+    resetEarnings(){
+        this.earningsTotal = 0;
+        this.mealCount = 0;
+        this.averageTip = 0;
+    }
 }
